f00b13ba24bbc2721a245f0817aaea408bfaca79
Iarlen <iarlensr@hotmail.com>
2024-05-23 17:37:35 -0300

    refactor: sorting projects by createdAt.

--- src/main/java/com/iarlen/reis/portfolioserver/repositories/ProjectRepository.java
+++ src/main/java/com/iarlen/reis/portfolioserver/repositories/ProjectRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<ProjectModel, UUID> {
-    Page<ProjectModel> findAllByTypeContainsAndActiveIsTrueAndOrderByCreatedAtDesc(String filter, Pageable pageable);
+    Page<ProjectModel> findAllByTypeContainsOrderByCreatedAtDescAndActiveIsTrue(String filter, Pageable pageable);
 
     Page<ProjectModel> findAllByTypeContainsOrderByCreatedAtDesc(String filter, Pageable pageable);
 }
